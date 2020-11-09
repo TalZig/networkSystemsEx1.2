aa3dcdcfd34d9538bd6004835696804dc1d82882
TalZig <Talz2347@gmail.com>
2020-11-09 22:55:57 +0200

    update some issues

--- server.py
+++ server.py
@@ -1,26 +1,31 @@
 import socket
 import sys
-import datetime
+import time
 
 myPort, parentIp, parentPort, ipsFileName = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('', myPort))
-file = open(ipsFileName,"r")
-lines = file.readline()
-ttls = []
-addresses = []
-ips = []
-dates = []
-for line in lines:
-	lineSplitByComma = line.split(sep=",")
-	addresses.append(line[0])
-	ips.append(line[1])
-	ttls.append(line[2])
-
 while True:
     data, addr = s.recvfrom(1024)
-	if (data in addresses):
-		s.sendto(str.encode(ips[addresses.index(data)]), addr)
-	else:
-		s.sendto()
-    s.sendto(data.upper(), addr)
+	file = open(ipsFileName, "r")
+	lines = file.readlines()
+	file.close()
+	isFound = False
+	for line in lines:
+		splitOfLine = line.split(sep=",")
+		if data == splitOfLine[0]:
+			if (len(splitOfLine) == 3 or float(splitOfLine[3]) - time.time()) < float(splitOfLine[2]):
+				s.sendto(str.encode(line), addr)
+				isFound = True
+			else:
+				lines.remove(line)
+		if not isFound:
+			s.sendto(data,(parentIp,parentPort))
+			data2, addr2 = s.recvfrom(1024)
+			data2 = data2.__str__() + ',' + str(time.time())
+			lines.append(data2)
+			file = open(ipsFileName, "w")
+			for line in lines:
+				file.write(line)
+			file.close()
+			s.sendto(bytes(data2), addr)
