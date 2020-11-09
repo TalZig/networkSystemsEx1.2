249851247716dd38fd0041f1a24d7a95a8e9cf96
TalZig <Talz2347@gmail.com>
2020-11-09 23:09:42 +0200

    update some issues

--- server.py
+++ server.py
@@ -4,9 +4,9 @@ import time
 
 myPort, parentIp, parentPort, ipsFileName = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('', myPort))
+s.bind(('', int(myPort)))
 while True:
-    data, addr = s.recvfrom(1024)
+	data, addr = s.recvfrom(1024)
 	file = open(ipsFileName, "r")
 	lines = file.readlines()
 	file.close()
@@ -20,6 +20,7 @@ while True:
 			else:
 				lines.remove(line)
 		if not isFound:
+
 			s.sendto(data,(parentIp,parentPort))
 			data2, addr2 = s.recvfrom(1024)
 			data2 = data2.__str__() + ',' + str(time.time())
@@ -28,4 +29,4 @@ while True:
 			for line in lines:
 				file.write(line)
 			file.close()
-			s.sendto(bytes(data2), addr)
+			s.sendto(data2.encode(), addr)
